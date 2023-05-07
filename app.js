06c80b7c3a2089b9e19dbbe2043b5905165442cd
latif <latifyousuf513@gmail.com>
2023-05-07 22:14:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// CHP # 35-38
+
+// Q # 1
+// function foo() {
+//     document.write(new Date())
+// }
+// foo()
+
+// Q # 2
+// function names(){
+//     var firstName = prompt("enter first name")
+//     var lastName = prompt("enter last name")
+//     var fullName = firstName + " " + lastName
+//     alert("Hello " + fullName)
+// }
+// names()
+
+// // Q # 3
+// function sum(){
+//     var firstNum = +prompt("enter first number")
+//     var lastNum = +prompt("enter last number")
+//     var add = firstNum + lastNum
+//     alert(add)
+// }
+// sum()
+
+
+
+// // Q # 4
+// function calculator(){
+//     var firstNum = +prompt("enter first number")
+//     var lastNum = +prompt("enter last number")
+//     var opt = prompt("enter operator")
+    
+//     if (opt === "+") {
+//         alert(firstNum + lastNum)
+//     }else if(opt === "-"){
+//         alert(firstNum - lastNum)
+//     }else if(opt === "*"){
+//         alert(firstNum * lastNum)
+//     }else if(opt === "/"){
+//         alert(firstNum / lastNum)
+//     }
+
+// calculator()
